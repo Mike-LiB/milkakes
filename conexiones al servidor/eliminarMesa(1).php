99ec2e0568adeaceb246a9d197f00683282949e8
Moises <109192499+moisescorado91@users.noreply.github.com>
2023-02-11 09:53:01 -0600

    Delete conexiones al servidor directory

--- conexiones al servidor/eliminarMesa(1).php
+++
@@ -1,8 +0,0 @@
-<?php
-    require("conexion.php");
-    $mesa = $_POST["mesa"];
-    mysqli_query($conexion,"DELETE FROM usuario WHERE Id_mesa=$mesa");
-    mysqli_query($conexion,"DELETE FROM mesa WHERE Id_mesa=$mesa");
-    //Codigo pendiente de allar la solucuion de no Eliminar de la tabla Orden  esta mesa
-    echo "exito";
-?>
--- conexiones al servidor/eliminarProducto(1).php
+++
@@ -1,9 +0,0 @@
-<?php
-    require("conexion.php");
-    $id = $_POST["id"];
-    mysqli_query($conexion,"DELETE FROM productosXpedidos WHERE id_producto='$id'");
-    mysqli_query($conexion,"DELETE FROM producto WHERE id_producto='$id'");
-    
-    //Codigo pendiente de allar la solucuion de no Eliminar de la tabla Orden  esta mesa
-    echo "exito";
-?>
