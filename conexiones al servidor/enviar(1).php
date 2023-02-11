dbe265b2e86519dc8493ef776293d3cbe9acd490
Moises <109192499+moisescorado91@users.noreply.github.com>
2023-02-11 09:52:42 -0600

    Delete enviar(1).php

--- conexiones al servidor/enviar(1).php
+++
@@ -1,9 +0,0 @@
-<?php
-    require("conexion.php");
-
-    $usu = $_POST["usu"];
-    $con = $_POST["contra"];
-
-    mysqli_query($conexion,"INSERT INTO usuarios(usu,contra)VALUES('$usu','$con')");
-    
-?>
